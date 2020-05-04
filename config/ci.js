df6741f89a6c220a4e2793db2c1ff8d80bdd9f4b
Alexey A <a.alkhutov@gmail.com>
2020-05-05 00:13:27 +0300

    fix typo in ci config

--- config/ci.js
+++ config/ci.js
@@ -3,5 +3,5 @@ module.exports = {
   googleClientSecret: process.env.googleClientSecret,
   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
-  redusUrl = 'redis://127.0.0.1:6379'
+  redusUrl: 'redis://127.0.0.1:6379'
 };
